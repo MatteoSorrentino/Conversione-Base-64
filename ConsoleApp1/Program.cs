b3ec53550dc349615aeaa0e084620066c9de7e30
Matteo Sorrentino <Ciro & Luisa@DESKTOP-1GLGU9U>
2018-12-18 15:39:49 +0100

    modifica del programma che sostituisce il tipo var con un array di byte

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,7 +14,7 @@ namespace ConsoleApp1
             Console.WriteLine("inserisci la stringa da convertire in base 64");
             plainText = Console.ReadLine(); ;
             string base64EncodedData;
-            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            byte[] plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
             for (int i = 0; i < plainTextBytes.Length; i++)
             {
                 Console.WriteLine(plainTextBytes[i]);
@@ -23,7 +23,7 @@ namespace ConsoleApp1
             base64EncodedData = Convert.ToBase64String(plainTextBytes);
             Console.WriteLine(base64EncodedData);
 
-            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            byte[] base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
             plainText = System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
             Console.WriteLine(plainText);
 
