2fb4cb1c43dc3a81c2fff303be9d5d590d41e0ee
Matteo Sorrentino <Ciro & Luisa@DESKTOP-1GLGU9U>
2018-12-18 15:42:41 +0100

    aggiunta funzionalità che permette di convertire un numero in un carattere ascii

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,14 +18,21 @@ namespace ConsoleApp1
             for (int i = 0; i < plainTextBytes.Length; i++)
             {
                 Console.WriteLine(plainTextBytes[i]);
-                Console.WriteLine(Convert.ToString(plainTextBytes[i]), 2);
+                Console.WriteLine(Convert.ToString(plainTextBytes[i], 2));
             }
             base64EncodedData = Convert.ToBase64String(plainTextBytes);
             Console.WriteLine(base64EncodedData);
 
             byte[] base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
             plainText = System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
-            Console.WriteLine(plainText);
+            Console.WriteLine($"la codifica in base 64 della parola {plainText} è {base64EncodedData}");
+            Console.WriteLine("");
+            int parola = 0;
+            Console.WriteLine("inserisci un numero da convertire in una carattere ascii");
+            Console.WriteLine("");
+            parola = Convert.ToInt32(Console.ReadLine());
+            char c = Convert.ToChar(parola);
+            Console.WriteLine($"il valore {parola} è trasformata in {c}");
 
             Console.ReadLine();
         }
